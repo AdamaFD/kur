5cac1dad9df9a9eb8f83eae8898539121f3fcbf7
AdamaFD <dsafonavap@gmail.com>
2026-02-10 09:46:12 +0300

    Update app2.js

--- app2.js
+++ app2.js
@@ -1,4 +1,4 @@
-// Простаяя SPA без фреймпыкииикт.
+// Простаяя SPA без фреймпыкииикты.
 // ПК: сайдбар + дерево + карта. Мобилка: список -> карта + drawer'ы.
 /* =========================
    APP STATE + DOM REFS
@@ -32,17 +32,7 @@ const mobileTree = document.getElementById("mobileTree");
 
 let cards = [];
 let currentCardId = null;
-let selectedNodes = new Set();
 
-let levelBranchCounts = {
-  3: {0:0, 1:0, 2:0}
-};
-
-let levelColumnCounts = {
-  4: {1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0},
-  5: {1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0},
-  6: {1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0}
-};
 /* =========================
    FIXED GRID 9 x 6
 ========================= */
@@ -199,6 +189,17 @@ function buildFixedTreeLayout(rootId) {
 /* =========================
    TREE RENDER
 ========================= */
+let selectedNodes = new Set();
+
+let levelBranchCounts = {
+  3: {0:0, 1:0, 2:0}
+};
+
+let levelColumnCounts = {
+  4: {1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0},
+  5: {1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0},
+  6: {1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0}
+};
 
 
 
